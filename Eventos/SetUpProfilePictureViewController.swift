e1b4a665f7e1deb67db44ff68c0220b8f7012ec1
Gabriel Mascarenhas <gabrielgb.mascarenhas@hotmail.com>
2017-11-08 00:10:17 -0800

    Pic set up done

--- Eventos/SetUpProfilePictureViewController.swift
+++ Eventos/SetUpProfilePictureViewController.swift
@@ -14,29 +14,41 @@ import MobileCoreServices
 import Fusuma
 
 
-class SetUpProfilePictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, FusumaDelegate {
+class SetUpProfilePictureViewController: UIViewController, FusumaDelegate {
     
-  
+    //MARK: - Declare Variables and inicializate View
+    
     @IBOutlet weak var chosenPictureButton: UIButton!
     @IBOutlet weak var skipButton: UIButton!
     private var imagePicker : UIImagePickerController!
+    @IBOutlet weak var takePicButton: UIButton!
     @IBOutlet weak var photoLibraryButton: UIButton!
     
     @IBOutlet weak var viewToBeTapped: UIView!
     @IBOutlet weak var heigthConstraint: NSLayoutConstraint!
     @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var changePicButton: UIButton!
+    
+    @IBOutlet weak var ImageheigthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var ImageWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var ImageXConstraint: NSLayoutConstraint!
+    @IBOutlet weak var ImageYConstraint: NSLayoutConstraint!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         heigthConstraint.constant = 0
         backgroundView.isHidden = true
+        takePicButton.addTopBorderWithColor(color: UIColor(red: 0, green: 0, blue: 0, alpha: 0.2), width: 0.5)
         photoLibraryButton.addTopBorderWithColor(color: UIColor(red: 0, green: 0, blue: 0, alpha: 0.2), width: 0.5)
         initialPic()
+        
+        ImageXConstraint.constant = self.view.frame.size.width/2 - 88
         
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
         tap.cancelsTouchesInView = false
         viewToBeTapped.addGestureRecognizer(tap)
+        
     }
     @objc func viewTapped(){
         UIView.animate(withDuration: 0.5, animations: {
@@ -48,107 +60,12 @@ class SetUpProfilePictureViewController: UIViewController, UIImagePickerControll
             }
         }
     }
-    func initialPic(){
-        let userID = Auth.auth().currentUser?.uid
-        let userDB = Database.database().reference().child("Users").child(userID!)
-        var picUrl : String = ""
-        
-        userDB.observeSingleEvent(of: .value) { (snapshot) in
-            let data = snapshot.value as! Dictionary<String,String>
-            picUrl = data["Photo"]!
-            print(data, picUrl)
-            if picUrl != ""{
-                
-                self.getPic(picUrl: picUrl)
-            }
-        }
-    }
-    @IBAction func photoLibraryChosen(_ sender: Any){
-        hide()
-        backgroundView.isHidden = true
-        let fusuma = FusumaViewController()
-        fusuma.delegate = self
-        fusuma.hasVideo = false
-        fusuma.cropHeightRatio = 1
-        fusuma.allowMultipleSelection = false
-        fusuma.defaultMode = .library
-        fusumaSavesImage = true
-        self.present(fusuma, animated: true, completion: nil)
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
-    @IBAction func dismissChoser(_ sender: Any) {
-        UIView.animate(withDuration: 0.5, animations: {
-            self.hide()
-            self.view.layoutIfNeeded()
-        }) { (canHide) in
-            if canHide{
-                self.backgroundView.isHidden = true
-            }
-        }
-    }
-    // MARK: AJdgjlsahgakdjhf
-    @IBAction func takePictureChosen(_ sender: Any){
-        hide()
-        backgroundView.isHidden = true
-        let fusuma = FusumaViewController()
-        fusuma.delegate = self
-        fusuma.hasVideo = false
-        fusuma.cropHeightRatio = 1
-        fusuma.allowMultipleSelection = false
-        fusuma.defaultMode = .camera
-        fusumaSavesImage = true
-        self.present(fusuma, animated: true, completion: nil)
-    }
-    
-    func fusumaImageSelected(_ image: UIImage, source: FusumaMode) {
-        makeButton(image: image)
-    }
-    func fusumaDismissedWithImage(_ image: UIImage, source: FusumaMode) {
-        
-    }
-    
-    func fusumaMultipleImageSelected(_ images: [UIImage], source: FusumaMode) {
-    }
-    
-    
-    func fusumaVideoCompleted(withFileURL fileURL: URL) {
-    }
-    
-    
-    func fusumaCameraRollUnauthorized() {
-        
-        print("Camera roll unauthorized")
-        
-        let alert = UIAlertController(title: "Access Requested",
-                                      message: "Saving image needs to access your photo album",
-                                      preferredStyle: .alert)
-        
-        alert.addAction(UIAlertAction(title: "Settings", style: .default) { (action) -> Void in
-            
-            if let url = URL(string:UIApplicationOpenSettingsURLString) {
-                
-                UIApplication.shared.openURL(url)
-            }
-        })
-        
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel) { (action) -> Void in
-            
-        })
-        
-        guard let vc = UIApplication.shared.delegate?.window??.rootViewController,
-            let presented = vc.presentedViewController else {
-                
-                return
-        }
-        
-        presented.present(alert, animated: true, completion: nil)
-    }
-    
-    func getPic(picUrl: String){
-        let url = URL(string: picUrl)
-        downloadImage(url: url!)
-    }
-    
+    //MARK: - Read and download and store URL
     func uploadMedia(completion: @escaping (_ url: String?) -> Void) {
         let storageRef = Storage.storage().reference().child("Users")
         if let uploadData = UIImagePNGRepresentation(self.chosenPictureButton.currentBackgroundImage!) {
@@ -163,6 +80,7 @@ class SetUpProfilePictureViewController: UIViewController, UIImagePickerControll
             }
         }
     }
+    
     func addToDB(){
         let userID = Auth.auth().currentUser?.uid
         let userDB = Database.database().reference().child("Users").child(userID!).child("Photo")
@@ -172,80 +90,50 @@ class SetUpProfilePictureViewController: UIViewController, UIImagePickerControll
             }
         }
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
     
-    @IBAction func changePictureFrom(_ sender: Any) {
-        UIView.animate(withDuration: 0.5) {
-            self.unhide()
-            self.view.layoutIfNeeded()
-        }
+    func initialPic(){
+        let userID = Auth.auth().currentUser?.uid
+        let userDB = Database.database().reference().child("Users").child(userID!)
+        var picUrl : String = ""
         
-    }
-    func changePicture(from: Int){
-        imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        
-        imagePicker.sourceType = .photoLibrary
-        if from == 1{
-            imagePicker.sourceType = .camera
-        }
-        else{
-            imagePicker.sourceType = .photoLibrary
-        }
-        imagePicker.allowsEditing = true
-        imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: imagePicker.sourceType)!
-        self.present(imagePicker, animated: true, completion: nil)
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        let mediaType = info[UIImagePickerControllerMediaType] as! String
-        
-        if mediaType == (kUTTypeImage as String){
-            let image = info[UIImagePickerControllerEditedImage] as! UIImage
-            makeButton(image: image)
-        }
-        
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    @IBAction func continuePressed(_ sender: Any) {
-        addToDB()
-    }
-    
-    func updateButton(){
-        
-    }
-    
-    
-    func getDataFromUrl(url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            completion(data, response, error)
-            }.resume()
-    }
-    func downloadImage(url: URL) {
-        print("Download Started")
-        getDataFromUrl(url: url) { data, response, error in
-            guard let data = data, error == nil else { return }
-            print(response?.suggestedFilename ?? url.lastPathComponent)
-            print("Download Finished")
-            DispatchQueue.main.async() {
-                self.makeButton(image: UIImage(data: data)!)
+        userDB.observeSingleEvent(of: .value) { (snapshot) in
+            let data = snapshot.value as! Dictionary<String,String>
+            picUrl = data["Photo"]!
+            if picUrl != ""{
+                self.getPic(picUrl: picUrl)
             }
         }
     }
     
-    func makeButton(image: UIImage){
-        
-        self.chosenPictureButton.setBackgroundImage(image, for: .normal)
-        self.chosenPictureButton.layer.cornerRadius = 88
-        self.chosenPictureButton.layer.masksToBounds = true
-        updateButton()
+    func getPic(picUrl: String){
+        let url = URL(string: picUrl)
+        downloadImage(url: url!)
+    }
+
+    func getDataFromUrl(url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            completion(data, response, error)}.resume()
+    }
+    func downloadImage(url: URL) {
+        getDataFromUrl(url: url) { data, response, error in
+            guard let data = data, error == nil else {
+                return }
+            DispatchQueue.main.async() {
+                self.makeButtonInitial(image: UIImage(data: data)!)
+            }
+        }
+    }
+
+    
+    //MARK: - Animation and Layout changes
+    
+    func prepareForAnimation(){
+        ImageXConstraint.constant = 0
+        ImageYConstraint.constant = 30
+        ImageWidthConstraint.constant = self.view.frame.size.width
+        ImageheigthConstraint.constant = self.view.frame.size.width
+        chosenPictureButton.layer.cornerRadius = self.view.frame.size.width/2
+        chosenPictureButton.layer.masksToBounds = true
     }
     
     func hide(){
@@ -255,43 +143,169 @@ class SetUpProfilePictureViewController: UIViewController, UIImagePickerControll
     }
     func unhide(){
         self.backgroundView.isHidden = false
-        self.backgroundView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2)
-        self.heigthConstraint.constant = 181
+        self.backgroundView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3)
+        self.heigthConstraint.constant = 236
         self.view.layoutIfNeeded()
         
     }
+    
+    func animateImage(){
+        ImageXConstraint.constant = self.view.frame.size.width - 88
+        ImageYConstraint.constant = 184
+        ImageWidthConstraint.constant = 176
+        ImageheigthConstraint.constant = 176
+        chosenPictureButton.layer.cornerRadius = ImageheigthConstraint.constant/2
+        chosenPictureButton.layer.masksToBounds = true
+    }
+    
+    //MARK: - Button and image related
+    
+    func makeButton(image: UIImage){
+        self.chosenPictureButton.setBackgroundImage(image, for: .normal)
+        UIView.animate(withDuration: 0.4) {
+            self.updateButton()
+            self.view.layoutIfNeeded()
+        }
+    }
+    func makeButtonInitial(image: UIImage){
+        self.chosenPictureButton.setBackgroundImage(image, for: .normal)
+        self.chosenPictureButton.layer.cornerRadius = 88
+        self.chosenPictureButton.layer.masksToBounds = true
+        UIView.animate(withDuration: 0.4) {
+            self.updateButton()
+            self.view.layoutIfNeeded()
+        }
+    }
+    func updateButton(){
+        if chosenPictureButton.currentBackgroundImage != nil{
+            let color = UIColor(red: 57, green: 222, blue: 191)
+            skipButton.setTitle("CONTINUAR", for: .normal)
+            skipButton.setTitleColor(color, for: .normal)
+            changePicButton.setTitle("ALTERAR FOTO", for: .normal)
+        }
+    }
+    func resetButton(){
+        let color = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2)
+        skipButton.setTitle("PULAR", for: .normal)
+        skipButton.setTitleColor(color, for: .normal)
+        changePicButton.setTitle("ADICIONAR FOTO", for: .normal)
+        chosenPictureButton.setBackgroundImage(UIImage(named: "Default Image") , for: .normal)
+    }
+    
+    //MARK: - Fusama Picker view related
+    
+    func fusumaImageSelected(_ image: UIImage, source: FusumaMode) {
+        makeButton(image: image)
+    }
+    func fusumaClosed() {
+        print("6789")
+        animateImage()
+        
+    }
+    func fusumaWillClosed() {
+        print("12345")
+        animateImage()
+    }
+    func fusumaDismissedWithImage(_ image: UIImage, source: FusumaMode) {
+        UIView.animate(withDuration: 1) {
+            self.animateImage()
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    func fusumaMultipleImageSelected(_ images: [UIImage], source: FusumaMode) {
+    }
+    
+    
+    func fusumaVideoCompleted(withFileURL fileURL: URL) {
+    }
+    
+    
+    func fusumaCameraRollUnauthorized() {
+    }
+    
+    //MARK: - Button action
+    
+    @IBAction func changePictureFrom(_ sender: Any) {
+        UIView.animate(withDuration: 0.5) {
+            self.unhide()
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    @IBAction func continuePressed(_ sender: Any) {
+        if skipButton.currentTitle == "CONTINUAR"{
+            addToDB()
+            performSegue(withIdentifier: "comFoto", sender: self)
+        }
+        if skipButton.currentTitle == "PULAR"{
+            print("CARINHA NAO QUIS FOTO")
+            performSegue(withIdentifier: "semFoto", sender: self)
+        }
+    }
+    
+    @IBAction func removePic(_ sender: Any) {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.hide()
+            self.view.layoutIfNeeded()
+        }) { (canHide) in
+            if canHide{
+                self.backgroundView.isHidden = true
+            }
+        }
+        UIView.animate(withDuration: 0.2) {
+            self.resetButton()
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @IBAction func photoLibraryChosen(_ sender: Any){
+        hide()
+        backgroundView.isHidden = true
+        let fusuma = FusumaViewController()
+        fusuma.delegate = self
+        fusuma.hasVideo = false
+        fusuma.cropHeightRatio = 1
+        fusuma.allowMultipleSelection = false
+        fusuma.defaultMode = .library
+        fusumaSavesImage = true
+        self.present(fusuma, animated: true, completion: {self.prepareForAnimation()})
+    }
+    
+    @IBAction func takePictureChosen(_ sender: Any){
+        hide()
+        backgroundView.isHidden = true
+        let fusuma = FusumaViewController()
+        fusuma.delegate = self
+        fusuma.hasVideo = false
+        fusuma.cropHeightRatio = 1
+        fusuma.allowMultipleSelection = false
+        fusuma.defaultMode = .camera
+        fusumaSavesImage = true
+        self.present(fusuma, animated: true, completion: {self.prepareForAnimation()})
+    }
+    
+    @IBAction func dismissChoser(_ sender: Any) {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.hide()
+            self.view.layoutIfNeeded()
+        }) { (canHide) in
+            if canHide{
+                self.backgroundView.isHidden = true
+            }
+        }
+    }
 }
+
+//MARK: - DONE
+
 extension UIButton {
     func addTopBorderWithColor(color: UIColor, width: CGFloat) {
         let border = CALayer()
         border.backgroundColor = color.cgColor
         border.frame = CGRect(x:0,y: 0, width:self.frame.size.width, height:width)
         self.layer.addSublayer(border)
-    }
-}
-extension UIImage{
-    
-    class func imageFromSystemBarButton(_ systemItem: UIBarButtonSystemItem, renderingMode:UIImageRenderingMode = .automatic)-> UIImage {
-        
-        let tempItem = UIBarButtonItem(barButtonSystemItem: systemItem, target: nil, action: nil)
-        
-        // add to toolbar and render it
-        let bar = UIToolbar()
-        bar.setItems([tempItem],animated: false)
-        bar.snapshotView(afterScreenUpdates: true)
-        
-        // got image from real uibutton
-        let itemView = tempItem.value(forKey: "view") as! UIView
-        
-        for view in itemView.subviews {
-            if view is UIButton {
-                let button = view as! UIButton
-                let image = button.imageView!.image!
-                image.withRenderingMode(renderingMode)
-                return image
-            }
-        }
-        
-        return UIImage()
     }
 }
